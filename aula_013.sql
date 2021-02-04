aa5672f434bdcbe21eb3efe475ec66d21fab3e74
oclaumc <49166205+oclaumc@users.noreply.github.com>
2021-02-04 15:57:00 -0300

    Update aula_013.sql

--- aula_013.sql
+++ aula_013.sql
@@ -30,3 +30,28 @@ select avg(carga) from cursos;
 select carga, count(*) from cursos where ano > 2015
 group by carga
 having carga > (select avg(carga) from cursos);
+
+
+/* 1-uma lista com as profissoes dos gafanhatos e seus respectivos quantitativos. */
+select profissao, count(*) from gafanhotos
+group by profissao desc;
+
+/* 2- Quantos gafanhotos homens e mulheres nasceram após 01/jan/2005 ? */
+select sexo,count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo;
+
+/* 3-Lista com gafanhotos que nasceram fora do BRASIL, mostrando o país de origem
+e o total de pessoas nascidas lá. Só nos interessam os países que tiveram mais de 3
+gafanhotos com essa nacionalidade. */
+select nacionalidade,count(*)from gafanhotos 
+where nacionalidade <> 'brasil'
+group by nacionalidade
+having count(nacionalidade ) >'3';
+
+/* 4-uma lista agrupada pela altura dos gafanhotos ,mostrando quantas pessoas 
+pesam mais de 100kg e que estao acima da media da altura de todos os gafanhotoso.*/
+select altura, peso, count(peso) from gafanhotos
+where peso > 100
+group by altura
+having altura > (select avg(altura) from gafanhotos where peso > 100);
